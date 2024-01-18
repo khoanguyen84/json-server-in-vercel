f90091a9b23fd58fd5953a6b853b471285313e89
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-01-18 09:24:21 +0700

    change configuration

--- api/server.js
+++ api/server.js
@@ -1,9 +1,6 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join('db.json')
@@ -11,13 +8,10 @@ const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
@@ -27,5 +21,4 @@ server.listen(8080, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
